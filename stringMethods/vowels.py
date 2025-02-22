2c7ad7d5d602370918608cd748dff52967a164fd
Vahablotfi <vahablotfi1@gmail.com>
2025-02-22 11:06:43 -0700

    vowel exercise added

---
+++ stringMethods/vowels.py
@@ -0,0 +1,11 @@
+"""
+Replacing Vowels in a String
+Explanation:
+We can replace characters in a string using loops and conditional statements. This example replaces all vowels in a string with *.
+
+Instructions:
+1. Ask the user to enter a sentence.
+2. Replace all vowels (a, e, i, o, u) with *.
+3. Print the modified sentence.
+
+"""
