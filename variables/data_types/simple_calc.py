acc7fa33d8ce42f7121a02edd76a112420307cb8
Vahablotfi <vahablotfi1@gmail.com>
2025-02-21 17:49:01 -0700

    simple calculations added

---
+++ variables/data_types/simple_calc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+--- 2: Performing Calculations with Variables ---
+
+Now that you’ve set up a savings variable, let’s take it a step further and use it to explore some calculations.
+
+Imagine you’re planning to save $10 every month. How much money will you have saved in four months? Rather than working with the numbers directly, let’s use variables to perform the calculation.
+
+Instructions
+1. Create a variable called monthly_savings and assign it the value 10.
+2. Define another variable, num_months, and set its value to 4.
+3. Multiply monthly_savings by num_months and store the result in a variable called new_savings.
+4. Finally, display the value of new_savings using the print() function.
+
+"""
+# Assign values to variables
+monthly_savings = 10
+num_months = 4 
+# Perform the calculation
+new_savings = monthly_savings * num_months
+
+# Print the result
+print(new_savings)
