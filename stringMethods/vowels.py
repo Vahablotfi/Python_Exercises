b943595d5997f8496db2e9f3a86fcaeca1007c86
Vahablotfi <vahablotfi1@gmail.com>
2025-02-28 15:30:51 -0700

    replacing vowels using exercise solved

--- stringMethods/vowels.py
+++ stringMethods/vowels.py
@@ -1,11 +1,34 @@
 """
 Replacing Vowels in a String
 Explanation:
-We can replace characters in a string using loops and conditional statements. This example replaces all vowels in a string with *.
+We can replace characters in a string using loops and conditional statements. 
+This example replaces all vowels in a string with *.
 
 Instructions:
 1. Ask the user to enter a sentence.
 2. Replace all vowels (a, e, i, o, u) with *.
 3. Print the modified sentence.
 
+Example Usage:
+    Input:  "Hello World"
+    Output: "H*ll* W*rld"
+
+    Input:  "AEIOU aeio u"
+    Output: "***** *****"
+
 """
+
+
+vowels = "auioeAUIOE"
+
+sentence = input("Please type a sentence : ")
+
+new_sentence = ''
+
+for char in sentence:
+    if char in vowels:
+        new_sentence += '*'
+    else:
+        new_sentence += char
+        
+print("modified sentence: ", new_sentence)
