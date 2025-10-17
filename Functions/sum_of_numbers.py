63742ade0243f0e0411d6e647bf661c37b858776
Vahablotfi <vahablotfi1@gmail.com>
2025-10-16 21:01:31 -0600

    sum of numbers exercise added

--- Functions/sum_of_numbers.py
+++ Functions/sum_of_numbers.py
@@ -16,3 +16,15 @@ In each iteration, add the current number to total.
 Return the final value of total.
 Test the function with several values of n (e.g., 5, 10, 100).
 """
+
+def sum_n_iterative(n):
+    total = 0
+    for i in range(1, n + 1):
+        total += i             
+    return total               
+
+
+# Testing the function
+print(sum_n_iterative(5))    # 1 + 2 + 3 + 4 + 5 = 15
+print(sum_n_iterative(10))   # 55
+print(sum_n_iterative(100))  # 5050
